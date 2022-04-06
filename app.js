0c4997daf2924cf7d4c4724f9d189e31a62d6b81
Rafael <92560766+FallenSentry@users.noreply.github.com>
2022-04-06 12:36:09 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const navSlide = ()=> {
+    const burger = document.querySelector('.logo');
+    const nav = document.querySelector('.nav-links');
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+    });
+}
+navSlide();
